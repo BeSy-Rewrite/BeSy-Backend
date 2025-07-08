73933a06a39191618b98780398ce57eca394e139
kr1pt0n05 <sandrolipinski@outlook.com>
2025-07-08 14:41:13 +0200

    Initial project setup with Spring Boot and Maven
    Add base project structure for a Spring Boot application, including Maven wrapper scripts, configuration files, Docker setup, and a basic application entry point with a test. Project dependencies and plugins are defined in pom.xml, and .gitignore is updated for common IDEs and build artifacts.

---
+++ src/main/java/de/hs_esslingen/besy/BeSyApplication.java
@@ -0,0 +1,13 @@
+package de.hs_esslingen.besy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeSyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeSyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/hs_esslingen/besy/BeSyApplicationTests.java
@@ -0,0 +1,13 @@
+package de.hs_esslingen.besy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeSyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
