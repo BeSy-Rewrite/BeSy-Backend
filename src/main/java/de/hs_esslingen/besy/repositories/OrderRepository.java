2d6d95629ccf28eb9443a52624357374931b9e27
kr1pt0n05 <sandrolipinski@outlook.com>
2025-08-16 20:42:02 +0200

    Remove legacy_user_name from API docs and unused import
    Deleted the legacy_user_name property from the UserResponseDTO in the API documentation and removed an unused List import from OrderRepository.java to clean up the codebase.

--- src/main/java/de/hs_esslingen/besy/repositories/OrderRepository.java
+++ src/main/java/de/hs_esslingen/besy/repositories/OrderRepository.java
@@ -4,8 +4,6 @@ import de.hs_esslingen.besy.models.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
 public interface OrderRepository extends JpaRepository<Order, Long> {
     Order findTopByPrimaryCostCenterIdAndBookingYearOrderByAutoIndexDesc(String primaryCostCenterId, String bookingYear);
