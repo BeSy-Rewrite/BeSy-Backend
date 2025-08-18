3a67e6933e28cdd52acd3f44254f1bee5caaf24b
kr1pt0n05 <sandrolipinski@outlook.com>
2025-08-18 21:22:57 +0200

    Integrate OAuth2 client and update security config
    Added OAuth2 client configuration for Keycloak in application.properties and updated RestClientConfig to use OAuth2AuthorizedClientManager for authenticated requests. SecurityConfigDev now uses issuer URI from properties and enables OAuth2 login. These changes improve authentication and authorization handling for REST client and resource server.

--- src/main/java/de/hs_esslingen/besy/configurations/RestClientConfig.java
+++ src/main/java/de/hs_esslingen/besy/configurations/RestClientConfig.java
@@ -1,17 +1,20 @@
 package de.hs_esslingen.besy.configurations;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientManager;
+import org.springframework.security.oauth2.client.web.client.OAuth2ClientHttpRequestInterceptor;
 import org.springframework.web.client.RestClient;
 
 @Configuration
 public class RestClientConfig {
 
     @Bean
-    public RestClient insyClient(RestClient.Builder restClientBuilder, @Value("${insy.api.base-url}") String URL) {
-        return restClientBuilder
-                .baseUrl(URL)
+    public RestClient restClient(OAuth2AuthorizedClientManager authorizedClientManager) {
+        OAuth2ClientHttpRequestInterceptor requestInterceptor =
+                new OAuth2ClientHttpRequestInterceptor(authorizedClientManager);
+        return RestClient.builder()
+                .requestInterceptor(requestInterceptor)
                 .build();
     }
 
