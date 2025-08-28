2d96b47a7bec64857f01b07de5bb66127f41d118
kr1pt0n05 <sandrolipinski@outlook.com>
2025-08-28 15:41:45 +0200

    Add task polling to Paperless PDF upload
    Enhanced the PaperlessController to poll for task completion after uploading a PDF, retrieving the document ID upon success. Added Jackson JSR310 dependency, updated application properties for new task URL and upload retry settings.

--- src/main/java/de/hs_esslingen/besy/controllers/PaperlessController.java
+++ src/main/java/de/hs_esslingen/besy/controllers/PaperlessController.java
@@ -1,6 +1,10 @@
 package de.hs_esslingen.besy.controllers;
 
-import org.apache.commons.io.IOUtils;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import de.hs_esslingen.besy.interfaces.paperless.Task;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.hc.client5.http.classic.methods.HttpGet;
 import org.apache.hc.client5.http.classic.methods.HttpPost;
 import org.apache.hc.client5.http.entity.mime.MultipartEntityBuilder;
 import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
@@ -9,26 +13,26 @@ import org.apache.hc.client5.http.impl.classic.HttpClients;
 import org.apache.hc.core5.http.ContentType;
 import org.apache.hc.core5.http.ParseException;
 import org.apache.hc.core5.http.io.entity.EntityUtils;
-import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.core.io.ByteArrayResource;
-import org.springframework.core.io.FileSystemResource;
-import org.springframework.core.io.InputStreamResource;
 import org.springframework.http.*;
-import org.springframework.http.client.MultipartBodyBuilder;
-import org.springframework.util.LinkedMultiValueMap;
-import org.springframework.util.MultiValueMap;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.client.RestClient;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.web.bind.annotation.*;;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.*;
+import java.util.List;
 
 
+@Slf4j
 @RestController
 @RequestMapping("${api.prefix}/paperless")
 public class PaperlessController {
+
+    @Value("${paperless.upload.max-attempts}")
+    private int MAX_RETRIES;
+
+    @Value("${paperless.upload.max-delay}")
+    private int MAX_DELAY;
 
     @Value("${paperless.api.base-url}")
     private String paperlessBaseUrl;
@@ -36,8 +40,18 @@ public class PaperlessController {
     @Value("${paperless.api.upload-url}")
     private String paperlessUploadUrl;
 
+    @Value("${paperless.api-task-url}")
+    private String paperlessTaskUrl;
+
     @Value("${paperless.api.token}")
     private String authToken;
+
+    @Autowired
+    private final ObjectMapper mapper;
+
+    public PaperlessController(ObjectMapper mapper) {
+        this.mapper = mapper;
+    }
 
 
     @PostMapping
@@ -54,10 +68,46 @@ public class PaperlessController {
             post.setEntity(builder.build());
 
             try (CloseableHttpResponse response = client.execute(post)) {
-                String responseString = EntityUtils.toString(response.getEntity());
-                return ResponseEntity.ok(responseString);
+                String uuid = EntityUtils.toString(response.getEntity()).replaceAll("\"", "");
+
+                if(response.getCode() == 200) {
+
+                        int retries = 0;
+                        String status = "";
+                        String documentId = "";
+
+                        while (!status.equals("SUCCESS") && retries < MAX_RETRIES){
+                            HttpGet getTask = new HttpGet(paperlessBaseUrl + paperlessTaskUrl + "?task_id=" + uuid);
+                            getTask.addHeader("Authorization", "Token " + authToken);
+
+                            try(CloseableHttpResponse taskResponse = client.execute(getTask)) {
+                                String responseBody = EntityUtils.toString(taskResponse.getEntity());
+                                List<Task> task = mapper.readValue(responseBody, new TypeReference<List<Task>>() {});
+
+                                documentId = task.get(0).getRelated_document();
+                                status = task.get(0).getStatus();
+                                retries++;
+                                Thread.sleep(MAX_DELAY);
+
+                            } catch (InterruptedException e) {
+                                log.atError().log("Fehler beim Schlafenlegen des Threads (PaperlessController - uploadPdf()) " + e.getMessage());
+                                throw new RuntimeException("Interner Serverfehler.");
+                            }
+                        }
+
+                        System.out.println("Status : " + status);
+                    System.out.println("DocumentId : " + documentId);
+
+                        if(status.equals("SUCCESS")) {
+                            System.out.println(documentId);
+                        } else{
+                            log.atError().log("Fehler beim Abrufen der Dokumenten-ID (PaperlessController - uploadPdf())");
+                            throw new RuntimeException("Interner Serverfehler.");
+                        }
+                }
             }
         }
+        return ResponseEntity.ok().build();
     }
 
 }
