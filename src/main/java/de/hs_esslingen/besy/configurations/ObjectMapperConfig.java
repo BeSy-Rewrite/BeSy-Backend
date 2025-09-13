51ec82e62ab3d775b915b19d3904ff026fbca52d
kr1pt0n05 <sandrolipinski@outlook.com>
2025-09-13 14:09:01 +0200

    Refactor ObjectMapper bean to use builder
    Updated ObjectMapperConfig to use Jackson2ObjectMapperBuilder for bean creation and module registration. This improves configuration consistency and leverages Spring's builder for ObjectMapper setup.

--- src/main/java/de/hs_esslingen/besy/configurations/ObjectMapperConfig.java
+++ src/main/java/de/hs_esslingen/besy/configurations/ObjectMapperConfig.java
@@ -4,15 +4,15 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 
 @Configuration
 public class ObjectMapperConfig {
 
     @Bean
-    public ObjectMapper objectMapper() {
-        ObjectMapper objectMapper = new ObjectMapper();
-        objectMapper.registerModule(new JavaTimeModule());
-        objectMapper.findAndRegisterModules();
-        return objectMapper;
+    public ObjectMapper objectMapper(Jackson2ObjectMapperBuilder builder) {
+        return builder
+                .modules(new JavaTimeModule())
+                .build();
     }
 }
