47e10e36155ff762d72151fc11fce3eae9cf85d5
kr1pt0n05 <sandrolipinski@outlook.com>
2025-09-27 17:32:40 +0200

    Update order export status check
    Changed the status check in exportOrder to require OrderStatus.SETTLED instead of OrderStatus.COMPLETED, ensuring only settled or archived orders can be exported.

--- src/main/java/de/hs_esslingen/besy/controllers/OrderController.java
+++ src/main/java/de/hs_esslingen/besy/controllers/OrderController.java
@@ -230,7 +230,7 @@ public class OrderController {
     @GetMapping("{order-id}/export")
     public ResponseEntity<byte[]> exportOrder(@PathVariable("order-id") Long orderId) throws IOException {
         if(!orderService.existsOrderById(orderId)) throw new NotFoundException("Bestellung nicht gefunden.");
-        if(!orderService.isOrderStatusEqual(orderId, List.of(OrderStatus.COMPLETED, OrderStatus.ARCHIVED))) throw new BadRequestException("Bestellstatus befindet sich nicht auf fertiggestellt!");
+        if(!orderService.isOrderStatusEqual(orderId, List.of(OrderStatus.SETTLED, OrderStatus.ARCHIVED))) throw new BadRequestException("Bestellstatus befindet sich nicht auf fertiggestellt!");
         return this.orderPDFService.generateOrderPDF(orderId);
     }
 
